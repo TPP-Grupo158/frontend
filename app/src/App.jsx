fec75f039da5ce67eae48d9c5f1ac9f27d8e97b7
Clemente <memeavila2001@gmail.com>
2026-03-06 15:44:25 -0300

    refactor: moved ImageUploadForm from App.jsx to ImageUploadForm.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,64 +1,12 @@
 import NiiVue from './components/Niivue/Niivue.jsx'
 import Login from './components/Login.jsx'
-import styles from './components/styles.js'
+import ImageUploadForm from './components/ImageUploadForm.jsx'
 import { useState } from 'react';
 import {
   BrowserRouter as Router,
-  Routes, Route, useNavigate
+  Routes, Route
 } from 'react-router-dom'
 
-const labels = ["Label 1", "Label 2", "Label 3"];
-
-const ImageUploadForm = () => {
-  const navigate = useNavigate();
-  const [images, setImages] = useState([]);
-  const [segmentationFile, setSegmentationFile] = useState(null);
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (!images.length) return;
-
-    navigate('/viewer', { 
-      state: { 
-        images, 
-        segmentationUrl: { file: segmentationFile }, 
-        labels 
-      } 
-    });
-  }
-
-  return (
-    <div style={styles.container}>
-      <form onSubmit={onFormSubmit} style={styles.form}>
-        <h2>Niivue file upload</h2>
-        <label>
-          Upload files
-          <input 
-            type="file" 
-            accept=".nii,.nii.gz" 
-            multiple 
-            onChange={(e) => {
-                const files = Array.from(e.target.files || []);
-                setImages(files.map(file => ({ file, name: file.name })));
-            }} 
-          />
-        </label>
-        <br />
-        <label>
-          Upload Segmentation:
-          <input 
-            type="file" 
-            accept=".nii,.nii.gz" 
-            onChange={(e) => setSegmentationFile(e.target.files[0])} 
-            />
-        </label>
-        <br />
-        <button type="submit"  style={styles.button}>Load Files</button>
-      </form>
-    </div>
-  )
-}
 
 function App() {
   const [_isLoggedIn, setIsLoggedIn] = useState(false);
