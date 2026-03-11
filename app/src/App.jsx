742295ef751d1c0c368feccb67bb7c937757c467
bmorselrtto <bmorseletto@fi.uba.ar>
2026-03-11 19:42:12 -0300

    added protection for routes and user persistance depedent on jwttoken cookie

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,11 @@
 import NiiVue from './components/Niivue/Niivue.jsx'
 import Login from './components/Login.jsx'
 import ImageUploadForm from './components/ImageUploadForm.jsx'
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import {
   BrowserRouter as Router,
-  Routes, Route
+  Routes, Route, Navigate, Outlet
 } from 'react-router-dom'
-
 
 function App() {
   const [_isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,12 +13,55 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/upload" element={<ImageUploadForm />} />
-        <Route path="/viewer" element={<NiiVue />} />
-        <Route path="/" element={<Login onLoginSuccess={() => setIsLoggedIn(true)} />} />
+        <Route path="/login" element={<Login onLoginSuccess={() => setIsLoggedIn(true)} />} />
+        <Route element={<ProtectedRoute />}>
+            <Route path="/upload" element={<ImageUploadForm />} />
+            <Route path="/viewer" element={<NiiVue />} />
+             <Route path="/" />
+        </Route>
+        
       </Routes>
     </Router>
   )
 }
+
+const ProtectedRoute = () => {
+  const [isValid, setIsValid] = useState(null); // null = checking, true = ok, false = fail
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_GATEWAY_API + "auth", {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: null,
+          credentials: 'include'
+        });
+
+        if (response.ok) {
+          setIsValid(true);
+        } else {
+          setIsValid(false);
+        }
+      } catch (error) {
+        console.error('Auth check failed:', error);
+        setIsValid(false);
+      }
+    };
+
+    verifyToken();
+  }, []);
+
+  if (isValid === null) {
+    return <div>Verifying session...</div>; 
+  }
+
+  if (isValid === false) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
 
 export default App
