42381ab000e8c28d2c6258f34d1c92392b7ba2b6
Clemente <memeavila2001@gmail.com>
2026-03-15 16:32:12 -0300

    added styles for PatientListPage

--- app/src/pages/PatientListPage.jsx
+++ app/src/pages/PatientListPage.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { usePatients } from '../hooks/usePatients'
 import { useDebounce } from '../hooks/useDebounce';
 
+import styles from '../components/styles';
+
 const DEBOUNCE_DELAY = 500; // ms
 
 
@@ -25,54 +27,65 @@ const PatientListPage = () => {
     fetchPatients('', debouncedNameFilter );
   }, [debouncedNameFilter]);
 
+
     return (
+      <div style={{padding: '0% 5%', display: 'flex', flexDirection: 'column'}}> 
+        <h1 style={{margin: '0 0 16px 0' }}>Patient Search</h1>
         <div>
-            <h1>Patient List</h1>
-            <div>
-              {currentFilters === 'dni' && 
-                <input 
-                  type="text"
-                  placeholder="Search by DNI"
-                  value={dniFilter}
-                  onChange={(e) => setDniFilter(e.target.value)}
-              />
-              }
-              {currentFilters === 'name' && 
-                <input 
-                  type="text"
-                  placeholder="Search by Name"
-                  value={nameFilter}
-                  onChange={(e) => setNameFilter(e.target.value)}
-              />
-              }
-              <select value={currentFilters} onChange={(e) => setCurrentFilters(e.target.value)}>
-                <option value="dni">DNI</option>
-                <option value="name">Name</option>
-              </select>
-            </div>
-            
-            {patients.length !== 0 && 
-              <table>
-                  <thead>
-                      <tr>
-                          <th>DNI</th>
-                          <th>Full Name</th>
-                          <th>Date of Birth</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      {patients.map(patient => (
-                          <tr key={patient.id}>
-                              <td>{patient.dni}</td>
-                              <td>{patient.fullname}</td>
-                              <td>{patient.date_of_birth}</td>
-                          </tr>
-                      ))}
-                  </tbody>
-              </table>
+          <div style={{display: 'flex', gap: '0.5rem', marginBottom: '4px'}}>
+            {currentFilters === 'dni' && 
+              <input 
+                type="text"
+                placeholder="Search by DNI"
+                value={dniFilter}
+                onChange={(e) => setDniFilter(e.target.value)}
+            />
             }
-            { patients.length === 0 && <p>No patients found.</p> }
+            {currentFilters === 'name' && 
+              <input 
+                type="text"
+                placeholder="Search by Name"
+                value={nameFilter}
+                onChange={(e) => setNameFilter(e.target.value)}
+            />
+            }
+            <select value={currentFilters} onChange={(e) => setCurrentFilters(e.target.value)}>
+              <option value="dni">DNI</option>
+              <option value="name">Name</option>
+            </select>
+          </div>
+          <table style={styles.table.container}>
+            <colgroup>
+              <col style={{ width: '20%' }} />{/* DNI */}
+              <col style={{ width: '50%' }} />{/* Full Name */}
+              <col style={{ width: '20%' }} />{/* Date of Birth */}
+            </colgroup>
+            <thead style={styles.table.header}>
+              <tr>
+                <th>DNI</th>
+                <th>Full Name</th>
+                <th>Date of Birth</th>
+              </tr>
+            </thead>
+            <tbody>
+              {patients.length !== 0 && patients.map((patient, index) => (
+                <tr key={patient.id} style={{...styles.table.row, backgroundColor: index % 2 === 0 ? '#f9f9f9' : 'white'}}>
+                  <td>{patient.dni}</td>
+                  <td>{patient.fullname}</td>
+                  <td>{patient.date_of_birth}</td>
+                </tr>
+              ))}
+              { patients.length === 0 && 
+              <tr>
+                <td></td>
+                <td style={{textAlign: 'center'}}>No patients found.</td>
+                <td></td>
+              </tr>
+              }
+            </tbody>
+          </table>
         </div>
+      </div>
     );  
 
 }
