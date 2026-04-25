4b88a26d839a2d8c2ec7274ff838d6dd900f17e6
Clemente <memeavila2001@gmail.com>
2026-04-25 15:50:58 -0300

    added test for navigation from PatienList to Patient page

--- app/tests/e2e-mocked/patient-list/navigation.spec.js
+++ app/tests/e2e-mocked/patient-list/navigation.spec.js
@@ -1,9 +1,27 @@
 import { test, expect } from '@playwright/test';
-import { userIsAuthenticated, mockResponse, waitForResponseWithUrl } from '../helpers';
+import { userIsAuthenticated, getMockPatients, mockResponse, waitForResponseWithUrl } from '../helpers';
 
 const API_URL = process.env.TEST_API_URL
 const BASE_URL = process.env.TEST_BASE_URL || 'http://localhost:5173';
 
+test('Clicking on patient View button navigates to patient details page', async ({ page }) => {
+  
+  const patients = getMockPatients(5, 'a')
+  await userIsAuthenticated(page);
+
+  await mockResponse(page, `${API_URL}/patients?offset=0&limit=10`, 200, { patients, hasMore: true });
+
+  const waitForPatientsResponse = waitForResponseWithUrl(page, `/patients?offset=0&limit=10`, 'GET');
+  await page.goto(`${BASE_URL}/patients`);
+  await waitForPatientsResponse
+
+  const patientRow = page.locator('table tbody tr').filter({ hasText: 'Patient' }).first();
+  const patientDNI = await patientRow.locator('td').first().innerText();
+  const viewButton = patientRow.getByRole('button', { name: 'View' });
+  await viewButton.click();
+
+  await expect(page).toHaveURL(`${BASE_URL}/patients/${patientDNI}`);
+});
 
 test('When user token expires its shown message to log in again', async ({ page }) => {
   await userIsAuthenticated(page);
