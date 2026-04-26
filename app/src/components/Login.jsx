70a98bd23c191e60f433717d30fe78549d78d255
Clemente <memeavila2001@gmail.com>
2026-04-26 15:16:06 -0300

    added error message on user login

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import styles from './styles.js'
 import PropTypes from 'prop-types';
+import Message from './Message.jsx';
 
 import { useNavigate } from 'react-router-dom';
 
 function Login({ onLoginSuccess }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [_error, setError] = useState('');
+  const [error, setError] = useState('');
   const [_loading, setLoading] = useState(false);
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const navigate = useNavigate();
 
@@ -16,6 +18,7 @@ function Login({ onLoginSuccess }) {
     e.preventDefault();
     setError('');
     setLoading(true);
+    setIsErrorVisible(false);
 
     try {
       const response = await fetch(import.meta.env.VITE_GATEWAY_API + "login", {
@@ -36,18 +39,26 @@ function Login({ onLoginSuccess }) {
         navigate('/patients');
       } else {
         setError(data.message || 'Invalid credentials');
+        setIsErrorVisible(true);
       }
     } catch {
       setError('Could not connect to the server.');
+      setIsErrorVisible(true);
     } finally {
       setLoading(false);
     }
   };
+
 return (
     <div style={styles.container}>
       <form onSubmit={handleSubmit} style={styles.form}>
-        <h2>Medical Image Processor</h2>
-        <p>Please sign in to continue</p>
+        <div>
+          <h2>Medical Image Processor</h2>
+          <p>Please sign in to continue</p>
+          {error && (
+            <Message isError message={error} visible={isErrorVisible} onClick={() => setIsErrorVisible(false)}/>
+          )}
+        </div>
         
         <input 
           type="email" 
