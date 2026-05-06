3ba2b3389dbf863dc31f1548f7d0021bc3bfc452
Clemente <memeavila2001@gmail.com>
2026-05-06 14:29:48 -0300

    refactor: extracted internalFetch and getErrorMessage from usePatients hook

--- app/src/hooks/usePatients.jsx
+++ app/src/hooks/usePatients.jsx
@@ -1,42 +1,12 @@
 import { useState } from 'react';
 
-const getErrorMessage = (data, response) => {
-  if (data && data.message) return data.message;
-  if (response.status >= 500) return 'There was a problem communicating with the server.';
-  return `An unknown error occurred (${response.status}).`;
-}
-  
+import { internalFetch, getErrorMessage } from './helpers';
+
 export const usePatients = () => {
     const [patients, setPatients] = useState([]);
     const [hasMorePages, setHasMorePages] = useState(false);
     const [error, setError] = useState({'message': '', status_code: null});
     const [loading, setLoading] = useState(false);
-
-    async function internalFetch(method, url,  body, onResponseSuccess, onResponseFailure) {
-
-      const headers = { Accept: 'application/json' };
-      if (body !== null) {
-        headers['Content-Type'] = 'application/json';
-      }
-      
-      const response = await fetch(
-        import.meta.env.VITE_GATEWAY_API + url,
-        {
-          method: method,
-          headers,
-          body: body ? JSON.stringify(body) : null,
-          credentials: 'include',
-        }
-      );
-      const data = await response.json();
-
-      if (response.ok) {
-        onResponseSuccess(data);
-      } else {
-        onResponseFailure(data, response);
-      }
-      return { data, response };
-    } 
 
     const fetchPatients = async (dni = '', name = '', offset = 0, limit = 10) => {
 
